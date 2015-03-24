ceb54cce397f1ff92cb484afb34bd7f3e520cfe3
Daigo UCHIYAMA <ucho@fuchidori.com>
2015-03-24 22:44:52 +0900

    add 9-8

---
+++ src/ch09/ex08/PointTest.java
@@ -0,0 +1,44 @@
+package ch09.ex08;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+class Point {
+
+    final private int x;
+    final private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int compareTo(Point other) {
+        if (x < other.x) {
+            return -1;
+        }
+        if (x > other.x) {
+            return 1;
+        }
+        if (y < other.y) {
+            return -1;
+        }
+        if (y > other.y) {
+            return 1;
+        }
+        return 0;
+    }
+}
+
+public class PointTest {
+
+    @Test
+    public void testCompareTo() {
+        assertTrue(0 > new Point(2, 3).compareTo(new Point(3, 2)));
+        assertTrue(0 < new Point(3, 2).compareTo(new Point(2, 3)));
+        assertTrue(0 > new Point(2, 2).compareTo(new Point(2, 3)));
+        assertTrue(0 < new Point(2, 3).compareTo(new Point(2, 2)));
+        assertTrue(0 == new Point(2, 3).compareTo(new Point(2, 3)));
+    }
+}
