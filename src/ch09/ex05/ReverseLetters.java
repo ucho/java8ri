a200259dbc91cc723668990b3e639ff12a6e0ce7
Daigo UCHIYAMA <ucho@fuchidori.com>
2015-03-24 22:09:40 +0900

    modify 9-5,9-6

--- src/ch09/ex05/ReverseLetters.java
+++ src/ch09/ex05/ReverseLetters.java
@@ -4,28 +4,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Stream;
-
-import org.junit.Test;
+import java.nio.file.StandardOpenOption;
 
 public class ReverseLetters {
 
-    public static Stream<Character> reverse(Path path) throws IOException {
-        byte[] bytes = Files.readAllBytes(path);
-        List<Character> charList = new ArrayList<>();
+    public static void convert(Path from, Path to) throws IOException {
+        byte[] bytes = Files.readAllBytes(from);
+        byte[] reversedBytes = new byte[bytes.length];
+        int pos = bytes.length - 1;
         for (byte b : bytes) {
-            charList.add((char) b);
+            reversedBytes[pos--] = b;
         }
-        Collections.reverse(charList);
-        return charList.stream();
+        Files.write(to, reversedBytes, StandardOpenOption.CREATE);
     }
 
-    @Test
-    public void example() throws Exception {
-        reverse(Paths.get("alice.txt")).forEach(System.out::print);
+    public static void main(String[] args) throws Exception {
+        convert(Paths.get("alice.txt"), Paths.get("/tmp/9-5.txt"));
     }
-
 }
---
+++ src/ch09/ex06/ReverseLines.java
@@ -0,0 +1,22 @@
+package ch09.ex06;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseLines {
+
+    public static void convert(Path from, Path to) throws IOException {
+        List<String> lines = Files.readAllLines(from);
+        Collections.reverse(lines);
+        Files.write(to, lines, StandardOpenOption.CREATE);
+    }
+
+    public static void main(String[] args) throws Exception {
+        convert(Paths.get("alice.txt"), Paths.get("/tmp/9-6.txt"));
+    }
+}
